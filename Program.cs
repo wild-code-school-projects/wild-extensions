2f37359986166e4a0d54e469d81d34f9884bdb7a
SuperCoolNinja <sbpro570@gmail.com>
2024-04-09 17:11:12 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Average(new int[] { 5, 50, 25, 10, 2 }, 5);
+            Console.WriteLine(sum);
+        }
+
+        public static int Average(int[] ints, int n)
+        {
+            return ints.Where(x => x <= n).Sum();
+        }
+    }
+}
